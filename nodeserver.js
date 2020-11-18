bfd57cfba61a0bbd4cd38e28072a2b9d5fe3b3f9
simplextech <info@simplextech.net>
2020-11-17 20:50:50 -0500

    Cleanup

--- nodeserver.js
+++ nodeserver.js
@@ -39,47 +39,20 @@ poly.on('config', function(config) {
   const nodesCount = Object.keys(config.nodes).length;
   logger.info('Config received has %d nodes', nodesCount);
 
-  // If we want to see the config content (Without the long nodes array):
-  // logger.info('Received config: %o',
-  //    Object.assign({}, config, { nodes: '<nodes>' }));
-
-  // Important config options:
-  // config.nodes: Our nodes, with the node class applied
-  // config.customParams: Configuration parameters from the UI
-  // config.newParamsDetected: Flag which tells us that customParams changed
-  // config.typedCustomData: Configuration parameters from the UI (if typed)
-
-  // If this is the first config after a node server restart
   if (config.isInitialConfig) {
-    // Removes all existing notices on startup.
     poly.removeNoticesAll();
 
-    // Use options specific to PGC vs Polyglot-V2
     if (poly.isCloud) {
       logger.info('Running nodeserver in the cloud');
-
-      // Will send the profile if the version is server.json is changed, or
-      // if the profile has never been sent. Exists only for PGC.
       poly.updateProfileIfNew();
     } else {
       logger.info('Running nodeserver on-premises');
-      // Profile files are sent automatically the first time.
-
-      // Sets the configuration fields in the UI / Available in Polyglot V2 only
-      // poly.saveTypedParams(typedParams);
-
-      // Sets the configuration doc shown in the UI
-      // Available in Polyglot V2 only
       const md = fs.readFileSync('./configdoc.md');
       poly.setCustomParamsDoc(markdown.toHTML(md.toString()));
     }
 
-    // Sets the configuration fields in the UI
     initializeCustomParams(config.customParams);
 
-    // If we have no nodes yet, we add the first node: a controller node which
-    // holds the node server status and control buttons The first device to
-    // create should always be the nodeserver controller.
     if (!nodesCount) {
       try {
         logger.info('Auto-creating controller');
@@ -88,14 +61,6 @@ poly.on('config', function(config) {
         logger.error('Error while auto-creating controller node:', err);
       }
     } else {
-      // Test code to remove the first node found
-
-      // try {
-      //   logger.info('Auto-deleting controller');
-      //  callAsync(autoDeleteNode(config.nodes[Object.keys(config.nodes)[0]]));
-      // } catch (err) {
-      //   logger.error('Error while auto-deleting controller node');
-      // }
     }
 
     if (config.newParamsDetected) {
@@ -106,12 +71,6 @@ poly.on('config', function(config) {
 
 poly.on('oauth', function(oAuth) {
   logger.info('Received OAuth code');
-  // oAuth object should contain:
-  // {
-  //   code: "<the authorization code to use to get tokens>"
-  //   state: "<the state worker you appended to the url>"
-  // }
-  // Use it to get access and refresh tokens
 });
 
 poly.on('poll', function(longPoll) {
@@ -119,37 +78,28 @@ poly.on('poll', function(longPoll) {
 });
 
 poly.on('oauth', function(oaMessage) {
-  // oaMessage.code: Authorization code received after authorization
-  // oaMessage.state: This must be the worker ID.
-
   logger.info('Received oAuth message %o', oaMessage);
-  // From here, we need to process the authorization token
 });
 
 poly.on('stop', async function() {
   logger.info('Graceful stop');
 
-  // Make a last short poll and long poll
   await doPoll(false);
   await doPoll(true);
 
-  // Tell Interface we are stopping (Our polling is now finished)
   poly.stop();
 });
 
 poly.on('delete', function() {
   logger.info('Nodeserver is being deleted');
-
-  // We can do some cleanup, then stop.
   poly.stop();
 });
 
 poly.on('mqttEnd', function() {
-  logger.info('MQTT connection ended.'); // May be graceful or not.
+  logger.info('MQTT connection ended.');
 });
 
 poly.on('messageReceived', function(message) {
-  // Only display messages other than config
   if (!message['config']) {
     logger.debug('Message Received: %o', message);
   }
@@ -160,7 +110,6 @@ poly.on('messageSent', function(message) {
 });
 
 async function doPoll(longPoll) {
-  // Prevents polling logic reentry if an existing poll is underway
   try {
     await lock.acquire('poll', function() {
       logger.info('%s', longPoll ? 'Long poll' : 'Short poll');
@@ -230,7 +179,6 @@ function callAsync(promise) {
 }
 
 function trapUncaughExceptions() {
-  // If we get an uncaugthException...
   process.on('uncaughtException', function(err) {
     logger.error(`uncaughtException REPORT THIS!: ${err.stack}`);
   });
